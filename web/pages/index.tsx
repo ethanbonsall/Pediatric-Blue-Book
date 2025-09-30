d4bffa1202d3570f961e6e756a11c119349dd99b
ethanbonsall <ethanbonsall50@gmail.com>
2025-09-30 01:28:22 -0400

    Completed preferences

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,6 +4,7 @@ import PBB from "../public/transparent-logo.png";
 import { supabase } from "@/lib/supabase";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 const Index = () => {
   const [signedIn, setSignedIn] = useState(false);
@@ -54,94 +55,100 @@ const Index = () => {
   };
 
   return (
-    <div>
-      {signedIn ? (
-        <div className="flex flex-col items-center w-full font-roboto min-h-screen bg-gradient-to-br from-background to-primary-300">
-          <NavBar />
-          <div className="flex flex-col h-screen" id="nutrient">
-            Nutrient Needs Calculator
-          </div>
-          <div className="flex flex-col h-screen" id="formula_calc">
-            Formula Recipe Calculator
-          </div>
-          <div className="flex flex-col h-screen" id="formula_lookup">
-            Formula Look Up Tool
-          </div>
-        </div>
-      ) : (
-        <div className="bg-gradient-to-tr from-primary to-background flex flex-col items-center justify-center w-full font-roboto min-h-screen">
-          <div className="flex flex-col text-center items-center justify-center h-screen">
-            <div className="flex flex-col items-center justify-center mb-[5dvh]">
-              <img
-                className="text-5xl md:text-6xl xl:text-[80px] w-[80dvw] lg:w-[25dvw] font-semibold text-white"
-                alt="Pediatric Blue Book"
-                src={PBB.src}
-              />
+    <>
+      <Head>
+        <title>Pediatric Blue Book</title>
+      </Head>
+      <div>
+        {signedIn ? (
+          <div className="flex flex-col items-center w-full font-roboto min-h-screen bg-gradient-to-br from-background to-primary-300">
+            <NavBar />
+            <div className="flex flex-col h-screen" id="nutrient">
+              Nutrient Needs Calculator
             </div>
-            <div className="flex flex-col w-fit h-fit bg-gradient-to-bl from-primary to-primary-600 justify-center text-white text-left items-center gap-6 p-8 rounded mb-[5dvh] shadow-2xl mx-1">
-              <div className="flex flex-col w-full gap-2">
-                <p className="text-4xl md:text-5xl font-bold text-white">
-                  Login
-                </p>
-                <p className="text-md md:text-xl">
-                  Enter your email and password below to login to your account.
-                </p>
-              </div>
-              <div className="flex flex-col text-left w-full">
-                <p className="text-2xl mb-2 text-white">Email</p>
-                <input
-                  className="rounded px-4 py-2 bg-white text-black w-full"
-                  autoComplete="email"
-                  name="email"
-                  onInput={(e) =>
-                    setEmail((e.target as HTMLInputElement).value)
-                  }
+            <div className="flex flex-col h-screen" id="formula_calc">
+              Formula Recipe Calculator
+            </div>
+            <div className="flex flex-col h-screen" id="formula_lookup">
+              Formula Look Up Tool
+            </div>
+          </div>
+        ) : (
+          <div className="bg-flow flex flex-col items-center justify-center w-full font-roboto min-h-screen">
+            <div className="flex flex-col text-center items-center justify-center h-screen">
+              <div className="flex flex-col items-center justify-center mb-[5dvh]">
+                <img
+                  className="text-5xl md:text-6xl xl:text-[80px] w-[80dvw] lg:w-[25dvw] font-semibold text-white"
+                  alt="Pediatric Blue Book"
+                  src={PBB.src}
                 />
               </div>
-              <div className="flex flex-col text-left w-full">
-                <div className="flex flex-row justify-between items-center">
-                  <p className="text-2xl mb-2 font-medium text-white">
-                    Password
+              <div className="flex flex-col w-fit h-fit bg-gradient-to-bl from-primary to-primary-600 justify-center text-white text-left items-center gap-6 p-8 rounded mb-[5dvh] shadow-2xl mx-1">
+                <div className="flex flex-col w-full gap-2">
+                  <p className="text-4xl md:text-5xl font-bold text-white">
+                    Login
                   </p>
-                  <Link
-                    className="text-md md:text-xl mb-1 md:mb-2 hover:underline"
-                    href="/forgot-password"
+                  <p className="text-md md:text-xl">
+                    Enter your email and password below to login to your
+                    account.
+                  </p>
+                </div>
+                <div className="flex flex-col text-left w-full">
+                  <p className="text-2xl mb-2 text-white">Email</p>
+                  <input
+                    className="rounded px-4 py-2 bg-white text-black w-full"
+                    autoComplete="email"
+                    name="email"
+                    onInput={(e) =>
+                      setEmail((e.target as HTMLInputElement).value)
+                    }
+                  />
+                </div>
+                <div className="flex flex-col text-left w-full">
+                  <div className="flex flex-row justify-between items-center">
+                    <p className="text-2xl mb-2 font-medium text-white">
+                      Password
+                    </p>
+                    <Link
+                      className="text-md md:text-xl mb-1 md:mb-2 hover:underline"
+                      href="/forgot-password"
+                    >
+                      Forgot your password?
+                    </Link>
+                  </div>
+                  <input
+                    className="rounded px-4 py-2 bg-white text-black w-full"
+                    autoComplete="password"
+                    name="password"
+                    type="password"
+                    onInput={(e) =>
+                      setPassword((e.target as HTMLInputElement).value)
+                    }
+                  />
+                </div>
+                <div className="flex flex-col w-full justify-center items-center gap-y-4">
+                  <button
+                    className="bg-secondary hover:bg-secondary-900 text-white px-4 py-2 rounded w-full transition-all duration-300"
+                    onClick={async () => {
+                      await handleSubmit();
+                    }}
                   >
-                    Forgot your password?
+                    Login
+                  </button>
+                  <Link
+                    className=" text-white text-md text-center hover:underline"
+                    href="/signup"
+                  >
+                    Sign Up
                   </Link>
                 </div>
-                <input
-                  className="rounded px-4 py-2 bg-white text-black w-full"
-                  autoComplete="password"
-                  name="password"
-                  type="password"
-                  onInput={(e) =>
-                    setPassword((e.target as HTMLInputElement).value)
-                  }
-                />
-              </div>
-              <div className="flex flex-col w-full justify-center items-center gap-y-4">
-                <button
-                  className="bg-secondary hover:bg-secondary-900 text-white px-4 py-2 rounded w-full transition-all duration-300"
-                  onClick={async () => {
-                    await handleSubmit();
-                  }}
-                >
-                  Login
-                </button>
-                <Link
-                  className=" text-white text-md text-center hover:underline"
-                  href="/signup"
-                >
-                  Sign Up
-                </Link>
               </div>
             </div>
+            <div></div>
           </div>
-          <div></div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- web/pages/reset-password.tsx
+++ web/pages/reset-password.tsx
@@ -1,53 +1,74 @@
 import { supabase } from "@/lib/supabase";
+import { X } from "lucide-react";
+import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 
-const Forgot = () => {
+const Reset = () => {
   const [Password, setPassword] = useState("");
+  const [showNotification, setShowNotification] = useState(false);
 
   async function updatePassword() {
     const { error } = await supabase.auth.updateUser({ password: Password });
     if (error) {
       alert(error.message);
+    } else {
+      setPassword("");
+      setShowNotification(true);
     }
   }
 
   return (
-    <div className="flex flex-col justify-center min-h-screen bg-gradient-to-br items-center from-primary-50 to-primary-400">
-      <div className="flex flex-col justify-center bg-background text-text w-fit h-fit shadow-2xl p-8">
-        <p className="text-center text-5xl font-bold">Password Reset?</p>
-        <form className="flex flex-col gap-y-6 pt-6 w-[500px] justify-center">
-          <label htmlFor="email" className="text-lg">
-            Enter your new password below to reset your password.
-          </label>
-          <input
-            type="password"
-            autoComplete="password"
-            id="password"
-            name="password"
-            placeholder="New Password"
-            className="border-b-2 rounded-md focus:outline-none"
-            onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
-          />
-          <button
-            type="submit"
-            className="bg-primary-600 text-background p-2 hover:bg-primary-700"
-            onClick={async (e) => {
-              e.preventDefault();
-              await updatePassword();
-            }}
-          >
-            Set New Password
+    <>
+      <Head>
+        <title>Reset Password</title>
+      </Head>
+      <div className="flex flex-col justify-center min-h-screen bg-gradient-to-br items-center from-primary-50 to-primary-400">
+        <div
+          className={`${
+            showNotification ? "fixed" : "hidden"
+          } top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full md:w-[50dvw] xl:w-[25dvw] bg-primary p-4 rounded text-md md:text-lg lg:text-xl z-50 gap-y-6`}
+        >
+          <button onClick={() => setShowNotification(false)}>
+            <X />
           </button>
-          <Link
-            href="/"
-            className="text-lg self-center text-black font-semibold hover:text-gray-800"
-          >
-            Back To Login
-          </Link>
-        </form>
+          <p>Password Reset Succesfully</p>
+        </div>
+        <div className="flex flex-col justify-center bg-background text-text w-fit h-fit shadow-2xl p-8">
+          <p className="text-center text-5xl font-bold">Password Reset?</p>
+          <form className="flex flex-col gap-y-6 pt-6 w-[500px] justify-center">
+            <label htmlFor="email" className="text-lg">
+              Enter your new password below to reset your password.
+            </label>
+            <input
+              type="password"
+              autoComplete="password"
+              id="password"
+              name="password"
+              placeholder="New Password"
+              className="border-b-2 rounded-md focus:outline-none"
+              onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
+            />
+            <button
+              type="submit"
+              className="bg-primary-600 text-background p-2 hover:bg-primary-700"
+              onClick={async (e) => {
+                e.preventDefault();
+                await updatePassword();
+              }}
+            >
+              Set New Password
+            </button>
+            <Link
+              href="/"
+              className="text-lg self-center text-black font-semibold hover:text-gray-800"
+            >
+              Back To Login
+            </Link>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
-export default Forgot;
+export default Reset;
